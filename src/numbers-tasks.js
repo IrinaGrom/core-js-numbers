8eedf81099cae18e967dea3edcc1e3d679fb3fc3
IrinaGrom <115471337+IrinaGrom@users.noreply.github.com>
2023-11-27 01:39:45 +0300

    fix: getSumOfNumbers

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -595,7 +595,7 @@ function getIntegerPartNumber(number) {
  * 0.1, 0.2, 0.3 => 0.6
  */
 function getSumOfNumbers(x1, x2, x3) {
-  return x1 + x2 + x3;
+  return Math.fround(x1 + x2 + x3).toFixed(1);
 }
 
 /**
